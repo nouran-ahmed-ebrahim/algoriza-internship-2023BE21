73397fb755861ad41c045a0d7b45f86aef3d51dd
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-28 06:48:09 +0200

    Create Specialization.cs

---
+++ Core/Domain/Specialization.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Core;
+using Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Specialization
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int NumOfRequests
+        {
+            get => Doctors.Sum(x => x.NumOfRequests);
+            private set { }
+        }
+        public List<Doctor> Doctors { get; set; }
+        //seeding
+    }
+}
