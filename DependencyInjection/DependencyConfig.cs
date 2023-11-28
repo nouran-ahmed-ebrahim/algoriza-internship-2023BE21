56fb550d2a074815f2615451cc289fba96457f78
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-28 04:40:20 +0200

    add some services

--- DependencyInjection/DependencyConfig.cs
+++ DependencyInjection/DependencyConfig.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Repository;
 
 namespace DependencyInjection
 {
     public static class DependencyConfig
     {
-        public static IServiceCollection ConfigureDependencies(IServiceCollection services)
+
+        public static IServiceCollection ConfigureDependencies(IServiceCollection Services)
         {
-
-            // Add more dependencies as needed
-
-            return services;
+            Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            Services.AddEndpointsApiExplorer();
+            return Services;
         }
     }
 }
