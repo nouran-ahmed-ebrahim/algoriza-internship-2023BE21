2ed8c7766f60da4e8da33040dae2e9a9f021ed79
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-28 06:41:21 +0200

    Create Doctor.cs

---
+++ Core/Domain/Doctor.cs
@@ -0,0 +1,30 @@
+﻿using Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int NumOfRequests
+        {
+            get => Requests.Select(r => r.RequestState == RequestState.Completed).Count();
+            private set { }
+        }
+        public Person Person { get; set; }
+        public Specialization Specialization { get; set; }
+        public List<Request> Requests { get; set; }
+        public List<Appointment> Appointments { get; set; }
+    }
+}
