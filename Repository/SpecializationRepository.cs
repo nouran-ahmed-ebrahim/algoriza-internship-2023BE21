0d5ab3f6b419d694936857473e778469332a9ee5
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-28 09:04:45 +0200

    add AddRangeAsync

---
+++ Repository/SpecializationRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Domain;
+using Core.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    internal class SpecializationRepository : BaseRepository<Specialization>, ISpecializationRepository
+    {
+        public SpecializationRepository(ApplictationDbContext context) : base(context)
+        {
+        }
+
+        public Task AddRangeAsync(List<Specialization> Specializations)
+        {
+            _context.Specializations.AddRange(Specializations);
+
+            _context.SaveChanges();
+            return Task.CompletedTask;
+        }
+    }
+}
