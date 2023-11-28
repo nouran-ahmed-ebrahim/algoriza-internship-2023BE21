88ad834768454cef63c5ffc42b50a6de281f3b8d
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-28 05:30:47 +0200

    Create Appointment.cs

---
+++ Core/Domain/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        public Doctor Doctor { get; set; }
+        public List<Appointment> Appointments { get; set; }
+
+    }
+}
