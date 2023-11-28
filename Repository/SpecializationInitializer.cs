440260bb42611b08b90915667f7987e6dc59089a
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-28 08:18:09 +0200

    Create SpecializationInitializer.cs

---
+++ Repository/SpecializationInitializer.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.CodeAnalysis;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Core.Domain
+{
+    internal class SpecializationInitializer
+    {
+        private List<Specialization> Specializations = new List<Specialization>
+        {
+            new Specialization
+            {
+                Name="Psychiatry(Mental, Emotional or Behavioral Disorders)"
+            },
+            new Specialization
+            {
+                Name="Dentistry(Teeth)"
+            },
+            new Specialization
+            {
+                Name="Pediatrics and New Born(Child)"
+            },
+           new Specialization
+            {
+                Name="Orthopedics(Bones)"
+            },
+            new Specialization
+            {
+                Name="Genecology and Infertility"
+            },
+            new Specialization
+            {
+                Name="Ear, Nose and Throat"
+            },
+           new Specialization
+            {
+                Name="Andrology and Male Infertility"
+            },
+            new Specialization
+            {
+                Name="Allergy and Immunology(Sensitivity and Immunity)"
+            },
+            new Specialization
+            {
+                Name="Cardiology and Vascular Disease(Heart)"
+            },
+           new Specialization
+            {
+                Name="Audiology"
+            },
+            new Specialization
+            {
+                Name="Cardiology and Thoracic Surgery(Heart & Chest)"
+            },
+            new Specialization
+            {
+                Name="Chest and Respiratory"
+            },
+            new Specialization
+            {
+                Name="Dietitian and Nutrition"
+            },
+            new Specialization
+            {
+                Name="Diagnostic Radiology(Scan Centers)"
+            },
+            new Specialization
+            {
+                Name="Diabetes and Endocrinology"
+            }
+        };
+        private ApplictationDbContext _context;
+
+        public SpecializationInitializer(ApplictationDbContext Context) { 
+            _context = Context;
+        }
+        public void Initialize(){
+            if (_context.Specializations.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            _context.Specializations.AddRange(Specializations);
+            
+            _context.SaveChanges();
+        }
+    }
+}
