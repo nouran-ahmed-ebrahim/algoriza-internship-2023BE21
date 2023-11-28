fa4569d00bd734b5ecf334847f70b3aa50ece590
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-28 06:26:38 +0200

    Create Request.cs

---
+++ Core/Domain/Request.cs
@@ -0,0 +1,28 @@
+﻿using Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public DateTime Time { get; set; }
+
+        [Required]
+        public DayOfWeek Day { get; set; }
+
+        [Required]
+        public RequestState RequestState { get; set; }
+
+        public Doctor Doctor { get; set; }
+        public Patient Patient { get; set; }
+        public DiscountCodeCoupon DiscountCodeCoupon { get; set; }
+    }
+}
