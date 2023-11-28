7ff3da81e9a2ffed7c3c5963142d5f10098fbe34
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-28 05:24:53 +0200

    Create AppointmentTime.cs

---
+++ Core/Domain/AppointmentTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class AppointmentTime
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Time { get; set; }
+
+        [ForeignKey("FK_AppointmentDayId")]
+        public AppointmentDay AppointmentDay { get; set; }
+    }
+}
