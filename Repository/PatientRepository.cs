0bce7ebdef556764d36474edab709e36561e504d
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 00:06:47 +0200

    Create PatientRepository.cs

---
+++ Repository/PatientRepository.cs
@@ -0,0 +1,44 @@
+﻿using Core.Domain;
+using Core.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    internal class PatientRepository : BaseRepository<ApplicationUser>, IBookingsRepository
+    {
+        public PatientRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+        public Task<ApplicationUser> AddAsync(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApplicationUser>> GetAllAsync(int Page, int PageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> NumOfPatients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUser> UpdateAsync(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
