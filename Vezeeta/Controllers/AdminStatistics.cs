68d1f435fd2b73dfbe0b26896772f70101d7559e
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 00:07:31 +0200

    Create AdminStatistics.cs

---
+++ Vezeeta/Controllers/AdminStatistics.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vezeeta.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/admin/statistics")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("Doctors")]
+        public Task<int> GetNumberOfDoctors()
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
