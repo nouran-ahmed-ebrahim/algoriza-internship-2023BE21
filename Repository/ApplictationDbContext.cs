9fd8903885ca15aa00d16b08f09459bb6626a08d
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-29 04:49:41 +0200

    Seeding specialization using EF

--- Repository/ApplictationDbContext.cs
+++ Repository/ApplictationDbContext.cs
@@ -20,6 +20,87 @@ namespace Repository
             modelBuilder.Entity<Specialization>()
             .HasIndex(p => p.Name)
             .IsUnique();
+
+            modelBuilder.Entity<Specialization>()
+             .HasData( new List<Specialization>
+                {
+                    new Specialization
+                    {
+                        Id =  1,
+                        Name="Psychiatry(Mental, Emotional or Behavioral Disorders)"
+                    },
+                    new Specialization
+                    {
+                        Id =  2,
+                        Name="Dentistry(Teeth)"
+                    },
+                    new Specialization
+                    {
+                        Id =  3,
+                        Name="Pediatrics and New Born(Child)"
+                    },
+                   new Specialization
+                    {
+                       Id =  4,
+                       Name="Orthopedics(Bones)"
+                    },
+                    new Specialization
+                    {
+                        Id =  5,
+                        Name="Genecology and Infertility"
+                    },
+                    new Specialization
+                    {
+                        Id =  6,
+                        Name="Ear, Nose and Throat"
+                    },
+                   new Specialization
+                    {
+                        Id =  7,
+                        Name="Andrology and Male Infertility"
+                    },
+                    new Specialization
+                    {
+                        Id =  8,
+                        Name="Allergy and Immunology(Sensitivity and Immunity)"
+                    },
+                    new Specialization
+                    {
+                        Id =  9,
+                        Name="Cardiology and Vascular Disease(Heart)"
+                    },
+                   new Specialization
+                    {
+                        Id = 10,
+                        Name="Audiology"
+                    },
+                    new Specialization
+                    {
+                        Id =  11,
+                        Name="Cardiology and Thoracic Surgery(Heart & Chest)"
+                    },
+                    new Specialization
+                    {
+                        Id = 12,
+                        Name="Chest and Respiratory"
+                    },
+                    new Specialization
+                    {
+                        Id =  13,
+                        Name="Dietitian and Nutrition"
+                    },
+                    new Specialization
+                    {
+                        Id =  14,
+                        Name="Diagnostic Radiology(Scan Centers)"
+                    },
+                    new Specialization
+                    {
+                        Id =  15,
+                        Name="Diabetes and Endocrinology"
+                    }
+                });
+
         }
         public DbSet<Person> Persons { get; set; }
        public DbSet<AppointmentDay> AppointmentDays { get; set; }
---
+++ Repository/Migrations/20231129024715_seedSpecializtaionsTable.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedSpecializtaionsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Specializations",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Psychiatry(Mental, Emotional or Behavioral Disorders)" },
+                    { 2, "Dentistry(Teeth)" },
+                    { 3, "Pediatrics and New Born(Child)" },
+                    { 4, "Orthopedics(Bones)" },
+                    { 5, "Genecology and Infertility" },
+                    { 6, "Ear, Nose and Throat" },
+                    { 7, "Andrology and Male Infertility" },
+                    { 8, "Allergy and Immunology(Sensitivity and Immunity)" },
+                    { 9, "Cardiology and Vascular Disease(Heart)" },
+                    { 10, "Audiology" },
+                    { 11, "Cardiology and Thoracic Surgery(Heart & Chest)" },
+                    { 12, "Chest and Respiratory" },
+                    { 13, "Dietitian and Nutrition" },
+                    { 14, "Diagnostic Radiology(Scan Centers)" },
+                    { 15, "Diabetes and Endocrinology" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 15);
+        }
+    }
+}
