84725ec4845e50dc212cc8ceff3f6c26f7ef73d9
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 01:01:05 +0200

    Update AdminStatistics.cs

--- Vezeeta/Controllers/AdminStatistics.cs
+++ Vezeeta/Controllers/AdminStatistics.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Core.Repository;
+using Core.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Repository;
+using Services;
 
 namespace Vezeeta.Controllers
 {
@@ -8,10 +12,30 @@ namespace Vezeeta.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private IUnitOfWork _unitOfWork;
+        private IBookingsServices _bookingsServices;
+
+        public ValuesController(IUnitOfWork unitOfWork, IBookingsServices bookingsServices) {
+            _unitOfWork = unitOfWork;
+            _bookingsServices = bookingsServices;
+        }
+        
         [HttpGet("Doctors")]
-        public Task<int> GetNumberOfDoctors()
+        public IActionResult GetNumberOfDoctors()
         {
-            return Task.FromResult(0);
+            return Ok(0);
+        }
+
+        [HttpGet("Patients")]
+        public IActionResult GetNumberOfPatients()
+        {
+            return Ok(0);
+        }
+
+        [HttpGet("Bookings")]
+        public IActionResult GetNumberOfBookings()
+        {
+            return Ok(_bookingsServices.NumOfBookings().Result);
         }
     }
 }
