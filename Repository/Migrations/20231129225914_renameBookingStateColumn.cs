842a4d21f8ea1b03dcfab9dec078575e17806a24
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 01:00:07 +0200

    rename booking state column

---
+++ Repository/Migrations/20231129225914_renameBookingStateColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class renameBookingStateColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RequestState",
+                table: "Bookings",
+                newName: "BookingState");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BookingState",
+                table: "Bookings",
+                newName: "RequestState");
+        }
+    }
+}
