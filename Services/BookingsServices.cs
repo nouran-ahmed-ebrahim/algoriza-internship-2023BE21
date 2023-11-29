dcf1c89c86862948fd68f1e689d1e51fc5ba3f8a
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 00:28:50 +0200

    Create BookingsServices.cs

---
+++ Services/BookingsServices.cs
@@ -0,0 +1,31 @@
+﻿using Core.Repository;
+using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+namespace Services
+{
+    public class BookingsServices : IBookingsServices
+    {
+        private IBookingRepository _bookingsRepository;
+
+        public BookingsServices(IBookingRepository BookingsRepository) {
+            _bookingsRepository = BookingsRepository;
+        }
+        public Task<IActionResult> NumOfBookings()
+        {
+            Task<int> totalBookings = _bookingsRepository.NumOfBooKings();
+            Task<int> pendingBookings= _bookingsRepository.NumOfBookings();
+            Task<int> completedBookings = _bookingsRepository.NumOfBookings();
+            Task<int> CancelledBookings = _bookingsRepository.NumOfBookings();
+
+            var result = new
+            {
+                TotalRequests = totalRequests,
+                PendingRequests = pendingRequests,
+                CompletedRequests = completedRequests
+            };
+            return
+        }
+    }
+}
