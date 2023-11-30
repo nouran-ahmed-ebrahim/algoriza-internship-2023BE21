70de78f71eced9496d0bbbcd75643d7ce9a50afd
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 03:10:24 +0200

    Create ApplicationUserRepository.cs

---
+++ Repository/ApplicationUserRepository.cs
@@ -0,0 +1,39 @@
+﻿using Core.Domain;
+using Core.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    internal class ApplicationUserRepository : BaseRepository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public ApplicationUserRepository(ApplicationDbContext context, UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager) : base(context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> GetUsersCountInRole(string roleName)
+        {
+            var role = await _roleManager.FindByNameAsync(roleName);
+            if (role == null)
+            {
+                return new NotFoundResult();
+            }
+
+            var usersInRole = await _userManager.GetUsersInRoleAsync(role.Name);
+            int userCount = usersInRole.Count;
+
+            return new OkObjectResult(userCount);
+        }
+    }
+}
