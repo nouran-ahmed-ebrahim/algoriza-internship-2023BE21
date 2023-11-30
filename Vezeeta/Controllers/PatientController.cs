843a4bcb5de2b785985ec58e6d33ca43e902a5f6
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 06:53:14 +0200

    chaneg addPationt Route to meet RESTFull Api

--- Vezeeta/Controllers/PatientController.cs
+++ Vezeeta/Controllers/PatientController.cs
@@ -7,7 +7,7 @@ using Services;
 
 namespace Vezeeta.Controllers
 {
-    [Route("api/PatientController")]
+    [Route("api/Patient")]
     [ApiController]
     public class PatientController : ControllerBase
     {
@@ -17,20 +17,20 @@ namespace Vezeeta.Controllers
             _applicationUserService = ApplicationUserService;
             _bookingsServices = bookingsServices;
         }
-        [HttpPost("Patient")]
+        [HttpPost]
         public async Task<IActionResult> AddPatient([FromBody]ApplicationUser user) 
         {
             //Image part
             // revert f,lasr name =>fullname
             // create userDTO Takes remeber me, first,lat, passwored
             // map(pass => hass, full name) pass rmem
-            // Validate the input data
-            // confermed pass
-            // act phone
-            // email val
-            // requierd from User table
-            // remove route
-            //  ore readable state code
+            // Validate the input data:
+            //   act phone
+            //   email val
+            //   requierd from User table
+            //more readable state code
+            // dont forget add add admin
+            // dont fforget uncoment cookie
             try
             {
                 if (!ModelState.IsValid)
@@ -44,5 +44,14 @@ namespace Vezeeta.Controllers
                 return StatusCode(500, $"An error occurred while adding the patient: {ex.Message}");
             }
         }
-    }
+
+  //      {
+  //"userName": "stwwwreeeesssing",
+  //"email": "www",
+  //"phoneNumber": "striwwng",
+  //"firstName": "ww",
+  //"lastName": "dd",
+  //"gender": 0,
+  //"dateOfBirth": "2023-11-30"}
+}
 }
