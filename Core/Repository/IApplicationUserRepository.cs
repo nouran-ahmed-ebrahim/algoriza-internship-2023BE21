ca9fd34b0e4f24e9e1b8a484aa064226a317719a
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 03:10:16 +0200

    Create IApplicationUserRepository.cs

---
+++ Core/Repository/IApplicationUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repository
+{
+    public interface IApplicationUserRepository: IBaseRepository<ApplicationUser>
+    {
+        public Task<IActionResult> GetUsersCountInRole(string roleName);
+    }
+}
