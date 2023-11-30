2d5ce629586a99ceb1609b09f652b77a6a33e96d
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 03:11:23 +0200

    Update IApplicationUserService.cs

--- Core/Services/IApplicationUserService.cs
+++ Core/Services/IApplicationUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Core.Services
 {
     public interface IApplicationUserService
     {
-        public Task<int> GetUsersCountInRole(string roleName);
+        public Task<IActionResult> GetUsersCountInRole(string roleName);
     }
 }
