b6298b7c9bb709709690acb1ec81fd1d880ce0bb
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-11-30 03:11:39 +0200

    Create ApplicationUserService.cs

---
+++ Services/ApplicationUserService.cs
@@ -0,0 +1,31 @@
+﻿using Core.Domain;
+using Core.Repository;
+using Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Services.ApplicationUserService;
+
+namespace Services
+{
+    public class ApplicationUserService: IApplicationUserService
+    {
+        private readonly IApplicationUserRepository _applicationUserRepository;
+
+        public ApplicationUserService(IApplicationUserRepository userRepository)
+        {
+            _applicationUserRepository = userRepository;
+        }
+
+        public async Task<IActionResult> GetUsersCountInRole(string roleName)
+        {
+            return await _applicationUserRepository.GetUsersCountInRole(roleName);
+        }
+    }
+
+
+}
