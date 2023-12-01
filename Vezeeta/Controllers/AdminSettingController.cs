6b245f8497ad1617c96605264916efdae47ff8f7
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-01 08:19:01 +0200

    Create AdminSettingController.cs

---
+++ Vezeeta/Controllers/AdminSettingController.cs
@@ -0,0 +1,20 @@
+﻿using Core.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vezeeta.Controllers
+{
+    [Route("Admin/DiscountCodeCoupon")]
+    [ApiController]
+    public class AdminSettingController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult DiscountCodeCoupon(DiscountCodeCoupon DiscountCodeCoupon)
+        {
+
+            return Ok();
+        }
+
+
+    }
+}
