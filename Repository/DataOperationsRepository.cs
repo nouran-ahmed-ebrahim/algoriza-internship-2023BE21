29880ff716a4f01c0a4281c44e0eb1b32693e150
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-01 07:16:35 +0200

    Create DataOperationsRepository.cs

---
+++ Repository/DataOperationsRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class DataOperationsRepository<T> : CommonRepository<T>, IDataOperationsRepository<T> where T : class
+    {
+        public DataOperationsRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public T Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            return entity;
+        }
+    }
+}
