34f17309b6ad0499ba114b14f18f1e1566698c5d
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-01 10:14:24 +0200

    add update

--- Services/DiscountCodeCouponServices.cs
+++ Services/DiscountCodeCouponServices.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Core.Domain;
+using Core.Repository;
+using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Services
 {
-    internal class DiscountCodeCouponServices
+    public class DiscountCodeCouponServices : IDiscountCodeCouponServices
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DiscountCodeCouponServices(IUnitOfWork UnitOfWork) {
+            _unitOfWork = UnitOfWork;
+        }
+
+        public IActionResult Add(DiscountCodeCoupon entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Deactivate(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Update(DiscountCodeCoupon entity)
+        {
+            var result = _unitOfWork.DiscountCodeCoupons.Update(entity);
+
+            _unitOfWork.Complete();
+            return result;
+        }
     }
 }
