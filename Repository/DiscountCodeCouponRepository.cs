94b8392485bf837aadae08390117e33a82dac411
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-01 09:56:13 +0200

    Create DiscountCodeCouponRepository.cs

---
+++ Repository/DiscountCodeCouponRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Domain;
+using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class DiscountCodeCouponRepository : DataOperationsRepository<DiscountCodeCoupon>, IDiscountCodeCouponRepository
+    {
+        public DiscountCodeCouponRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public void Deactivate(int id)
+        {
+            _context;
+        }
+    }
+}
