6260890d978c982f6fd17ef2a569e0d9dd576bc4
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-01 07:20:18 +0200

    Create DoctorRepository.cs

---
+++ Repository/DoctorRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Domain;
+using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    internal class DoctorRepository : CommonRepository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
