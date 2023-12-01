6db328fbed34cfd5e13f57072d15566afe5d0410
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-01 07:07:32 +0200

    Create ICommonRepository.cs

---
+++ Core/Repository/ICommonRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repository
+{
+    public interface ICommonRepository<T> : IBaseRepository<T> where T : class
+    {
+        T UpdateAsync(T entity);
+        void DeleteAsync(T entity);
+    }
+}
