441e7c109d294767bf0bc6f296b58504b9a93661
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-01 07:07:36 +0200

    Create CommonRepository.cs

---
+++ Repository/CommonRepository.cs
@@ -0,0 +1,28 @@
+﻿using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CommonRepository<T> : BaseRepository<T>, ICommonRepository<T> where T : class
+    {
+        public CommonRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public void DeleteAsync(T entity)
+        {
+             _context.Set<T>().Remove(entity);
+        }
+
+        public T UpdateAsync(T entity)
+        {
+            _context.Update(entity); 
+            return entity;
+        }
+    }
+}
