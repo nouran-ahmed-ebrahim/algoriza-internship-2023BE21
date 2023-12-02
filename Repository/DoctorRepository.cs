b2f3e18a185e43a3e55c51d5b565e17b37542fd5
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-02 15:02:08 +0200

    Create doctorRepo

---
+++ Repository/DoctorRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Domain;
+using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class DoctorRepository : DataOperationsRepository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+        _unitOfWork._.Entry<Doctor>((doctor).Reference(g => g.Gender).Load();
+    }
+}
