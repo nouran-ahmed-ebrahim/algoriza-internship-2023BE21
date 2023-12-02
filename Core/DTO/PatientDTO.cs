79c2c083547e687d064de0d74d8a17281ac5459b
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-02 08:42:13 +0200

    Create PatientDTO.cs

---
+++ Core/DTO/PatientDTO.cs
@@ -0,0 +1,48 @@
+﻿using Core.Utilities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class PatientDTO
+    {
+        public int Id { get; set; }
+
+        [RequiredAttributeForDoctor(ErrorMessage = "This property is required for users with the 'doctor' role.")]
+        public IFormFile? Image { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(".+@.+\\.com", ErrorMessage= "Email is not valid")]
+        public string Email { get; set; }
+
+        [Required] 
+        public string Password { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]+$", ErrorMessage ="Phone number is not valid")]
+        public string Phone { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(Gender))]
+        public Gender Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        
+        [Required]
+        public bool RememberMe {  get; set; }   
+
+    }
+}
