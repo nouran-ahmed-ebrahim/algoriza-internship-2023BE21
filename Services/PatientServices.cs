6b8eced665690c846ed708f74d00010ac0b131c0
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-02 08:43:32 +0200

    Create PatientServices.cs

---
+++ Services/PatientServices.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Core.Domain;
+using Core.DTO;
+using Core.Repository;
+using Core.Services;
+using Core.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class PatientServices(IUnitOfWork UnitOfWork, IMapper mapper) : ApplicationUserService(UnitOfWork, mapper), IPatientServices
+    {
+        public Task<IActionResult> Add(PatientDTO userDTO, UserRole userRole, bool rememberMe)
+        {
+            ApplicationUser user = _mapper.Map<ApplicationUser>(userDTO);
+            return Add(user, userRole, rememberMe);
+        }
+    }
+}
