99dff436ee41211e02ac156cab3d3ec0a7973892
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-02 08:19:55 +0200

    Create Add doctor Api

---
+++ Vezeeta/Controllers/AdminDoctorController.cs
@@ -0,0 +1,40 @@
+﻿using Core.DTO;
+using Core.Repository;
+using Core.Services;
+using Core.Utilities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vezeeta.Controllers
+{
+    [Route("api/Admin/Doctor")]
+    [ApiController]
+    public class AdminDoctorController : ControllerBase
+    {
+        private readonly IApplicationUserService _applicationUserService;
+
+        public AdminDoctorController(IApplicationUserService applicationUserService)
+        {
+            _applicationUserService = applicationUserService;
+        }
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> AddDoctor([FromForm] UserDTO userDTO)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                };
+
+                return await _applicationUserService.Add(userDTO, UserRole.Patient, userDTO.RememberMe);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while adding the patient: {ex.Message}");
+            }
+        }
+    }
+}
