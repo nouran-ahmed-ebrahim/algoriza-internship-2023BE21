fcc8bdf9d9cdbbbdc725a030f15b09ee79e717d8
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-02 08:44:12 +0200

    add add method

--- Core/Services/IPatientServices.cs
+++ Core/Services/IPatientServices.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.DTO;
+using Core.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,7 @@ namespace Core.Services
 {
     public interface IPatientServices:IApplicationUserService
     {
+        public Task<IActionResult> Add(PatientDTO userDTO, UserRole userRole, bool rememberMe);
+
     }
 }
