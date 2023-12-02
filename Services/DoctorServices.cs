d0a114b490a23016587c65a72e3125c0ed0408bf
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-02 08:55:56 +0200

    Create DoctorServices.cs

---
+++ Services/DoctorServices.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Core.Repository;
+using Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class DoctorServices : ApplicationUserService, IDoctorServices
+    {
+        public DoctorServices(IUnitOfWork UnitOfWork, IMapper mapper) : base(UnitOfWork, mapper)
+        {
+        }
+    }
+}
