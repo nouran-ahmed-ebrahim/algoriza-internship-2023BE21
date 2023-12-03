4f5856ac51212bc1932c1523d005a50ffeb9d822
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-03 07:47:26 +0200

    implement signIn & logout APIs

--- Vezeeta/Controllers/AdminSettingController.cs
+++ Vezeeta/Controllers/AdminSettingController.cs
@@ -4,6 +4,7 @@ using Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Services;
+using System.Text.RegularExpressions;
 
 namespace Vezeeta.Controllers
 {
@@ -12,10 +13,13 @@ namespace Vezeeta.Controllers
     public class AdminSettingController : ControllerBase
     {
         private readonly IDiscountCodeCouponServices _discountCodeCouponServices;
+        private readonly IApplicationUserService _adminServices;
 
-        public AdminSettingController(IDiscountCodeCouponServices DiscountCodeCouponServices) 
+        public AdminSettingController(IDiscountCodeCouponServices DiscountCodeCouponServices,
+            IApplicationUserService AdminServices) 
         {
             _discountCodeCouponServices = DiscountCodeCouponServices;
+            _adminServices = AdminServices;
         }
 
         [HttpPut]
@@ -76,6 +80,41 @@ namespace Vezeeta.Controllers
 
             return _discountCodeCouponServices.Deactivate(id);
 
+        }
+
+        [HttpGet("SignIn")]
+        public async Task<IActionResult> SignIn(string Email, string Password, bool RememberMe)
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                ModelState.AddModelError("Email", "Email is required");
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                ModelState.AddModelError("Password", "Password is required");
+            }
+
+            string pattern = ".+@.+\\.com";
+            bool isMatch = Regex.IsMatch(Email, pattern);
+
+            if (!isMatch)
+            {
+                ModelState.AddModelError("Email", "Invalid Email");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return await _adminServices.SignIn(Email, Password, RememberMe);
+        }
+
+        [HttpPost("LogOut")]
+        public async Task<IActionResult> LogOut()
+        {
+            await _adminServices.SignOut();
+            return Ok("LogOut Successfully");
         }
     }
 }
