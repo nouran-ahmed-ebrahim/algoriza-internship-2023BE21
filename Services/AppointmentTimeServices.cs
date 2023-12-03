8f90cde8871ca84655e2942af745a0851cef735c
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-03 13:18:55 +0200

    implement AddingDayTime

--- Services/AppointmentTimeServices.cs
+++ Services/AppointmentTimeServices.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Core.Domain;
+using Core.Repository;
+using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,30 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-    public class AppointmentTimeServices
+    public class AppointmentTimeServices : IAppointmentTimeServices
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public AppointmentTimeServices(IUnitOfWork _unitOfWork) { 
+        }
+        public IActionResult AddingDayTime(int dayId, DateTime timeSlot)
+        {
+            try
+            {
+                AppointmentTime appointmentTime = new AppointmentTime()
+                {
+                    AppointmentId = dayId,
+                    Time = timeSlot
+                };
+                _unitOfWork.AppointmentTimes.Add();
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult($"There is a problem while adding time slot " +
+                    $"{timeSlot} \n {ex.Message} : \n {ex.InnerException?.Message}");
+            }
+
+            
+        }
     }
 }
