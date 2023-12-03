149f976eb6724b98c4f754eef0af6d0d76044a46
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-03 12:59:06 +0200

    inject AppointmentTimeServices & AppointmenServices

--- DependencyInjection/DependencyConfig.cs
+++ DependencyInjection/DependencyConfig.cs
@@ -32,6 +32,8 @@ namespace DependencyInjection
 
             Services.AddTransient<IUnitOfWork, UnitOfWork>();
             Services.AddTransient<IApplicationUserService, ApplicationUserService>();
+            Services.AddTransient<IAppointmentTimeServices, AppointmentTimeServices>();
+            Services.AddTransient<IAppointmentServices, AppointmentServices>();
             Services.AddTransient<IPatientServices, PatientServices>();
             Services.AddTransient<IDoctorServices, DoctorServices>();
             Services.AddTransient<IBookingsServices, BookingsServices>();
