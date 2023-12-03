27a7115463f52ebd6910c2ffbf96b6a886d198d3
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-03 12:03:11 +0200

    implement GetNextDoctorId

---
+++ Repository/AppointmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Domain;
+using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AppointmentRepository(ApplicationDbContext context) :
+            BaseRepository<Appointment>(context), IAppointmentRepository
+    {
+        public int GetNextDoctorId()
+        {
+            var maxId = _context.Appointments.Select(d => d.Id).DefaultIfEmpty(0).Max();
+            return (maxId + 1);
+        }
+    }
+}
