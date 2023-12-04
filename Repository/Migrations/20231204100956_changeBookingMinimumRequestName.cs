e9af27c315e06daed326b4acd6c4964b9355d21d
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-04 12:13:24 +0200

    Rename Column MinimumRequiredRequests

---
+++ Repository/Migrations/20231204100956_changeBookingMinimumRequestName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeBookingMinimumRequestName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MinimumRequiredRequests",
+                table: "DiscountCodeCoupons",
+                newName: "MinimumRequiredBookings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MinimumRequiredBookings",
+                table: "DiscountCodeCoupons",
+                newName: "MinimumRequiredRequests");
+        }
+    }
+}
