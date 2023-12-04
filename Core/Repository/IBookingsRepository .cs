7c84853bf4c6656cf17b5d838acbc0d8a4c554ab
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-04 09:00:36 +0200

    remove unnecessary access modifiers

--- Core/Repository/IBookingsRepository .cs
+++ Core/Repository/IBookingsRepository .cs
@@ -1,18 +1,11 @@
 ﻿using Core.Domain;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Repository
 {
     public interface IBookingsRepository: ICommonRepository<Booking>
     {
-        public int NumOfBooKings();
-        public int NumOfBookings(Expression<Func<Booking, bool>> criteria);
+        int NumOfBooKings();
+        int NumOfBookings(Expression<Func<Booking, bool>> criteria);
     }
 }
