c236ce3254141509106cc94bcf5f0f5dec5ae8b4
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-04 07:53:04 +0200

    Add IsExist

--- Core/Repository/ICommonRepository.cs
+++ Core/Repository/ICommonRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,7 @@ namespace Core.Repository
     public interface ICommonRepository<T> : IBaseRepository<T> where T : class
     {
         IActionResult Update(T entity);
+        bool IsExist(Expression<Func<T, bool>> criteria);
+
     }
 }
--- Repository/CommonRepository.cs
+++ Repository/CommonRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Core.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,10 @@ namespace Repository
 
         }
 
+        public bool IsExist(Expression<Func<T, bool>> criteria) 
+        {
+            return _context.Set<T>().Any(criteria);
+        }
         public IActionResult Update(T entity)
         {
             try
