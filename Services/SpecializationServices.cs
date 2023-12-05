1c49d2a4e5d8a1f929f1eb531d49685564cde81b
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-05 11:28:39 +0200

    implement GetTop5 in services

---
+++ Services/SpecializationServices.cs
@@ -0,0 +1,19 @@
+﻿using Core.Repository;
+using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection
+{
+    public class SpecializationServices : ISpecializationServices
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public SpecializationServices(IUnitOfWork unitOfWork) {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult GetTop5()
+        {
+            return _unitOfWork.Specializations.GetTop5();
+        }
+    }
+}
