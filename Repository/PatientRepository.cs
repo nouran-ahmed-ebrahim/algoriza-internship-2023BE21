da4f27729a46c1e7102220aac69bebb2a0a48467
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-06 15:06:58 +0200

    Create PatientRepository.cs

---
+++ Repository/PatientRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Domain;
+using Core.Repository;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class PatientRepository : ApplicationUserRepository, IPatientRepository
+    {
+        public PatientRepository(ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager) : 
+            base(context, userManager, roleManager, signInManager)
+        {
+        }
+    }
+}
