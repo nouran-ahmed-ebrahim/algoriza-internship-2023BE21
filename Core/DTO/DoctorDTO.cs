31c5a8f0d9fca317dc7b4ce44477c6b0034c8176
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-06 11:10:38 +0200

    add Image Atribute

--- Core/DTO/DoctorDTO.cs
+++ Core/DTO/DoctorDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,13 @@ namespace Core.DTO
 {
     public class DoctorDTO
     {
-        public string Image;
+        public string ImagePath;
         public string FullName;
         public string Email;
         public string Phone;
         public string Gender;
         public DateTime BirthOfDate;
         public string Specialization;
+        public Image? Image;
     }
 }
