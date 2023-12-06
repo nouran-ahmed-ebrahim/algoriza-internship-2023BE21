09917892bdb6e6f9e9798461db34de03ab2abe2d
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-06 17:16:33 +0200

    Create PatientDTO.cs

---
+++ Core/DTO/PatientDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class PatientDTO
+    {
+        public string ImagePath;
+        public string FullName;
+        public string Email;
+        public string Phone;
+        public string Gender;
+        public string DateOfBirth;
+        public Image? Image;
+    }
+}
