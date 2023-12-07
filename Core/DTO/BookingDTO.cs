2c2ab0b4891778e4c04f57d2e37e4a438a7e1418
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-07 08:32:32 +0200

    Create BookingDTO.cs

---
+++ Core/DTO/BookingDTO.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities;
+using System.Drawing;
+
+namespace Core.DTO
+{
+    public class BookingDTO
+    {
+       public Image? Image;
+       public string ImagePath;
+       public string doctorName;
+       public string specialization;
+       public string day;
+       public string time;
+       public string price;
+       public string discoundCodeName;
+       public int CouponValue;
+       public DiscountType DiscountType;
+       public int finalPrice;
+       public string BookingStatus;
+    }
+}
