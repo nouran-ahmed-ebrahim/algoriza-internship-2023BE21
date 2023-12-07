182ce01879a476fcc89e69be16baba058983c7cb
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-07 21:55:35 +0200

    Create BookingWithPatientDTO.cs

---
+++ Core/DTO/BookingWithPatientDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class BookingWithPatientDTO
+    {
+        public PatientDTO patientInfo;
+        public string day;
+        public string time;
+    }
+}
