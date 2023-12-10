4842dc3ff78d46f705ae02b6cf1308b64f08515d
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-10 13:19:27 +0200

    Create Delete all dataBase data.sql

---
+++ Delete all dataBase data.sql
@@ -0,0 +1,7 @@
+delete from Bookings
+delete from Appointments
+delete from AppointmentTimes
+delete from Doctors 
+delete from AspNetUsers
+
+DBCC CheckIdent(Bookings, RESEED,0)DBCC CheckIdent(Appointments, RESEED,0)DBCC CheckIdent(AppointmentTimes, RESEED,0)DBCC CheckIdent(Doctors, RESEED,0)
