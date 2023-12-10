96760b8097ba32c67d2eb0cf2f05d4b05dd2ff99
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2023-12-10 13:19:52 +0200

    delete nav prosperity

--- Core/Domain/Appointment.cs
+++ Core/Domain/Appointment.cs
@@ -24,6 +24,5 @@ namespace Core.Domain
         [ForeignKey("FK_Appointments_Doctors_DoctorId")]
         [AllowNull]
         public virtual int DoctorId { get; set; }
-        //public virtual List<AppointmentTime>? AppointmentTimes { get; set; }
     }
 }
